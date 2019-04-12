b365796d4efbdd6b415582210275c67cf2ec63d8
Joe Andaluz <jandaluz1@gmail.com>
2019-04-12 15:37:29 -0400

    added GameContainer component to group games by status: live/upcoming/final

--- client/src/components/GameDay.js
+++ client/src/components/GameDay.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Scoreboard from './Scoreboard';
+import GameContainer from './GameContainer';
 
 export default class GameDay extends Component {
   state = { games: [] };
@@ -21,11 +22,30 @@ export default class GameDay extends Component {
   render() {
     const { games } = this.state;
     // console.log(games);
+    const liveGames = games.filter(
+      game =>
+        game.status.status === 'In Progress' || game.status.status === 'Warmup'
+    );
+    const upcomingGames = games.filter(
+      game =>
+        game.status.status === 'Pre-Game' || game.status.status === 'Preview'
+    );
+    const finals = games.filter(
+      game =>
+        game.status.status === 'Final' ||
+        game.status.status === 'Game Over' ||
+        game.status.status === 'Postponed'
+    );
+    console.log(liveGames, upcomingGames, finals);
     return (
       <div>
         <h1>GameDay</h1>
-        {games[0] &&
-          games.map(game => <Scoreboard game={game} key={game.id} />)}
+        <GameContainer status="live" games={liveGames} />
+        <GameContainer status="upcoming" games={upcomingGames} />
+        <GameContainer status="final" games={finals} />
+        {/* {games.map(game => (
+          <Scoreboard game={game} key={game.id} />
+        ))} */}
       </div>
     );
   }
